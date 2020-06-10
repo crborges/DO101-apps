42c9667e6a52abe8eaf03b09eaf57218bd3ee36f
Cristiano Rodrigues Borges <cristianoborges.com@gmail.com>
2020-06-09 21:55:02 -0300

    Versao 2.o do app com coisas de php junto

--- version/app.js
+++ version/app.js
@@ -4,13 +4,19 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-
-    response = 'This is version 1 of the app.' + '\n';
-
-    //send the response to the client
+    response = 'This is version 2 of the app.' + '\n';
     res.send(response);
-
 });
+
+
+
+app.get('/index.php', function (req, res) {
+    response = 'Só parece que é php mais naum eh' + '\n';
+    res.send(response);
+});
+
+
+
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
